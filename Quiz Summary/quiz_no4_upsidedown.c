57fa084d284758bfca4b19a83adb365b23d35854
Fickry Imamsyah <142564782+yozoyaa@users.noreply.github.com>
2023-11-09 12:40:10 +0700

    Update quiz_no4_upsidedown.c

--- Quiz Summary/quiz_no4_upsidedown.c
+++ Quiz Summary/quiz_no4_upsidedown.c
@@ -5,9 +5,9 @@ int main() {
     int total_lines = 5;
 
     // Outer loop for each line
-    for (int i = 7; i > 0; --i) {
+    for (int i = 1; i < 7; ++i) {
         // Inner loop to print numbers in descending order starting from 9
-        for (int j = 5; j >= 7 - i + 1; --j) {
+        for (int j = i; j <= 5; ++j) {
             printf("%3d ", j);
         }
         // Newline after each row is printed
@@ -18,9 +18,9 @@ int main() {
 }
 
 /*Outputnya: 
-  5   4   3   2   1 
-  5   4   3   2 
-  5   4   3 
-  5   4 
-  5
+  1   2   3   4   5 
+  2   3   4   5 
+  3   4   5 
+  4   5 
+  5 
 */
